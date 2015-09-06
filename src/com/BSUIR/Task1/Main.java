1e7a8ef342da8c9f69cea441d9f7184516633dd1
Vladislav Zavadski <vladislav.zavadski@gmail.com>
2015-09-06 16:03:25 +0300

    Initial commit

---
+++ src/com/BSUIR/Task1/Main.java
@@ -0,0 +1,10 @@
+package com.BSUIR.Task1;
+
+/**
+ * Created by Владислав on 06.09.2015.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.print("hello");
+    }
+}
